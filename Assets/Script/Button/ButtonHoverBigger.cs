82a0bf642d987db29c1711589910bee111303566
TuND <tumoonhanh@gmail.com>
2023-03-03 23:58:20 +0700

    test commit

--- Assets/Script/Button/ButtonHoverBigger.cs
+++ Assets/Script/Button/ButtonHoverBigger.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ButtonHoverBigger : MonoBehaviour
-{
+{   //comment for test commit
     public float normalSize = 1f; // the normal size of the button
     public float enlargedSize = 1.2f; // the size of the button when it's hovered over
     private Vector3 normalScale; // the scale vector for the normal size
