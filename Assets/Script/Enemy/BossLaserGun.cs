9e6f5175aecd91a1f07143889aa950ad0fff33e1
TuND <tumoonhanh@gmail.com>
2023-03-17 15:09:14 +0700

    fix: laser beam spawn and countdown

--- Assets/Script/Enemy/BossLaserGun.cs
+++ Assets/Script/Enemy/BossLaserGun.cs
@@ -2,71 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
 public class BossLaserGun : MonoBehaviour
 {
-    public GameObject LaserBeam;
+    public float durationTime = 10;
     public float currentTime;
-    public int range = 2;
     public int damage = 2;
     public LayerMask enemyLayerMask;
     public LayerMask playerLayerMask;
     private GameObject countdownText;
-    public float durationTime;
+    
 
     void Start()
     {
         currentTime = durationTime;
         enemyLayerMask = LayerMask.GetMask("Enemy");
-        UnityEngine.Debug.Log("Start");
-        // Countdown text
-        countdownText = new GameObject("myText");
-        countdownText.AddComponent<TextMeshPro>();
-        countdownText.AddComponent<MeshRenderer>();
-        UnityEngine.Debug.Log("before textmesh");
-        TextMeshPro textMeshComponent = countdownText.GetComponent<TextMeshPro>();
-        MeshRenderer meshRendererComponent = countdownText.GetComponent<MeshRenderer>();
-        textMeshComponent.text = currentTime.ToString("F1");
-        textMeshComponent.color = Color.black;
-        textMeshComponent.fontSize = 5;
-        textMeshComponent.sortingOrder = 10;
-        textMeshComponent.alignment = TextAlignmentOptions.Center;
-        UnityEngine.Debug.Log("aftertextmesh");
-
-        // add bomb range:
+        UnityEngine.Debug.Log("Start create Laser Object");
+        // add laser range:
         var lineRender = gameObject.AddComponent<LineRenderer>();
         UnityEngine.Debug.Log("LineRender");
         lineRender.material = new Material(Shader.Find("Legacy Shaders/Particles/Alpha Blended Premultiply"));
         lineRender.sortingOrder = 10;
-        var drawCircle = gameObject.AddComponent<DrawCircleController>();
-        drawCircle.range = range;
+        var drawRectangle = gameObject.AddComponent<DrawRectangleController>();
     }
-    // Update is called once per frame
-    //void Update()
-    //{
-    //    if (isActive)
-    //    {
-    //        timer -= Time.deltaTime;
 
-    //        if (timer <= 0.0f)
-    //        {
-    //            isActive = false;
-    //            timer = duration;
-    //            laserBeam.SetActive(false);
-    //        }
-    //    }
-    //}
-
-    // Method to activate the laser beam
-    //public void ActivateLaserBeam()
-    //{
-    //    isActive = true;
-    //    laserBeam.SetActive(true);
-    //}
-
-    //// Method to deactivate the laser beam
-    //public void DeactivateLaserBeam()
-    //{
-    //    isActive = false;
-    //    laserBeam.SetActive(false);
-    //}
+    public void Update()
+    {
+        UnityEngine.Debug.Log("Process turn off laser");
+        currentTime -= Time.deltaTime;
+        UnityEngine.Debug.Log(currentTime);
+      //  countdownText.GetComponent<TextMeshPro>().text = currentTime.ToString("F1");
+      //  countdownText.transform.position = transform.position;
+        if (currentTime <= 0)
+        {
+            UnityEngine.Debug.Log("start turn off laser");
+            TurnOffLaser();
+        }
+        
+        //if (durationTime <= 0)
+        //{
+        //    Destroy(countdownText);
+        //    Destroy(gameObject);
+        //}
+    }
+    private void TurnOffLaser()
+    {
+        //var list = Physics2D.OverlapCircleAll((Vector2)transform.position, range, enemyLayerMask);
+        //UnityEngine.Debug.Log("buum hit " + list.Length);
+        //foreach (var enemy in list)
+        //{
+        //    Debug.Log(enemy.gameObject.name);
+        //    enemy.gameObject.GetComponent<EnemyObject>().IsHit(damage);
+        //}
+        //Destroy(countdownText);
+        Destroy(gameObject);
+    }
 }
